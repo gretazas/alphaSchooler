be3c7f21732ad39c660d32fd1f0c8836dee575be
gretazas <esminis@yahoo.com>
2022-12-12 22:34:28 +0000

    Sort responsive max-width 430px

--- static/script/script.js
+++ static/script/script.js
@@ -5,15 +5,15 @@ function handleTabletChange(mediaQuery) {
     if (mediaQuery.matches) {
         document.getElementById('responsive').innerHTML = `
         <div class="col-sm-12 text-center card-body ml-5">
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">1</div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">2</div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">3</div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">4</div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">5</div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">6</div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">7</div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">8</div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card">9</div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>&nbsp; &nbsp; Copies/ Diaries<i class="fa-solid fa-book mx-3"></i></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Pencils<i class="fa-solid fa-pencil mx-3"></i></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Stationary<img src="/media/stationary.png" style="height:20px;width:30px;padding-left:10px;"></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Lunch Boxes<i class="fa-solid fa-box mx-3"></i></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Bags<img src="/media/bag.jpg" style="height:100px; width:120px;"></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Pencil Cases<img src="/media/pencil-case.png" style="height:20px;width:30px;padding-left:10px;"></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>All Products<i class="fa-solid fa-bag-shopping fa-lg mx-3"></i></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Art Suplies<i class="fa-solid fa-paintbrush mx-3"></i></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Folders<img src="/media/folder.png" style="height:30px;width:30px;padding-left:10px;"></a></div>
     </div>`
     }
 }
