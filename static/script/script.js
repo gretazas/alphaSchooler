da6997dc0969e750dd5da79f9328cb33b9cf62ac
gretazas <esminis@yahoo.com>
2022-12-13 11:46:02 +0000

    Make navbar smaller, add alpha pic infront Schooler logo

--- static/script/script.js
+++ static/script/script.js
@@ -5,7 +5,7 @@ function handleTabletChange(mediaQuery) {
     if (mediaQuery.matches) {
         document.getElementById('responsive').innerHTML = `
         <div class="col-sm-12 text-center card-body ml-5">
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>&nbsp; &nbsp; Copies/ Diaries<i class="fa-solid fa-book mx-3"></i></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card h-25"><a>&nbsp; &nbsp; Copies/ Diaries<i class="fa-solid fa-book mx-3"></i></a></div>
             <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Pencils<i class="fa-solid fa-pencil mx-3"></i></a></div>
             <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Stationary<img src="/media/stationary.png" style="height:20px;width:30px;padding-left:10px;"></a></div>
             <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Lunch Boxes<i class="fa-solid fa-box mx-3"></i></a></div>
@@ -13,7 +13,7 @@ function handleTabletChange(mediaQuery) {
             <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Pencil Cases<img src="/media/pencil-case.png" style="height:20px;width:30px;padding-left:10px;"></a></div>
             <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>All Products<i class="fa-solid fa-bag-shopping fa-lg mx-3"></i></a></div>
             <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Art Suplies<i class="fa-solid fa-paintbrush mx-3"></i></a></div>
-            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card"><a>Folders<img src="/media/folder.png" style="height:30px;width:30px;padding-left:10px;"></a></div>
+            <div class="row w-50 mx-5 d-flex align-items-center justify-content-center card h-25"><a>Folders<img src="/media/folder.png" style="height:30px;width:30px;padding-left:10px;"></a></div>
     </div>`
     }
 }
