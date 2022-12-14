e3b16eb41f370ee9d7c50f76ebb65be42cd303d2
gretazas <esminis@yahoo.com>
2022-12-14 17:17:45 +0000

    Add product_detail.html

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -7,4 +7,11 @@ def all_products(request):
     products = Product.objects.all()
 
     return render(request, 'products/products.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    """ Show individual product """
+    product = get_object_or_404(Product, pk=product_id)
+
+    return render(request, 'products/product_detail.html', {'product': product})
 
