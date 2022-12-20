09dca26f6fc8e7b9be5bc9c27c5f401d11c45192
gretazas <esminis@yahoo.com>
2022-12-20 12:18:19 +0000

    Add category filtering

--- products/views.py
+++ products/views.py
@@ -5,8 +5,20 @@ from .models import Product
 def all_products(request):
     """ Show all products """
     products = Product.objects.all()
+    categories = None
 
-    return render(request, 'products/products.html', {'products': products})
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+    context = {
+        'current_categories': categories,
+        'products': products
+    }
+
+    return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
