7c7995f179fe07d222e8cc47eb7d19203424e590
gretazas <esminis@yahoo.com>
2023-01-03 16:33:06 +0000

    Add config vars in Heroku, settings.py

--- alphaSchooler/settings.py
+++ alphaSchooler/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mva_cm3jdz!&^&nawn=wq+cndlyn(@5shxfn5d^y79lak4_ne7'
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
