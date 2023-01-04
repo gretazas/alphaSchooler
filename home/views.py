2e5593c06811262ca89bbd2db7b607a172cbda6c
gretazas <esminis@yahoo.com>
2023-01-04 08:20:37 +0000

    Make ruler in base.html sticky

--- home/views.py
+++ home/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
     """ A view to return the index page """
+    products = Product.objects.all()
+    categories = None
 
-    return render(request, 'home/index.html')
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+    context = {
+        'current_categories': categories,
+        'products': products
+    }
+
+    return render(request, 'home/index.html', context)
