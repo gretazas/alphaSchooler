220b3268a19e9bf4ed8dadd7fc0abed672f7c05d
gretazas <esminis@yahoo.com>
2023-01-11 07:14:08 +0000

    Add checkout card functionality

--- checkout/static/js/stripe.elements.js
+++ checkout/static/js/stripe.elements.js
@@ -21,4 +21,67 @@ var style = {
 
 card.mount('#card-element');
 
-console.log('gooogogogo')
+// Handle validation errors
+
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    });
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+
+    var url = '/checkout/cache_checkout_data/';
+
+    
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+        
+        }).fail(function () {
+        location.reload();
+        })
+    
--- checkout/views.py
+++ checkout/views.py
@@ -6,9 +6,16 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem
 
 from products.models import Product
+from bag.contexts import bag_contents
+import stripe
+import json
 
 
 def checkout(request):
+    ''' Render checkout page '''
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     bag = request.session.get('bag', {})
 
@@ -17,12 +24,22 @@ def checkout(request):
         return redirect(reverse('products'))
 
     order_form = OrderForm()
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+    order_form = OrderForm()
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': 'pk_test_51MOKnEGn2B9VBfJ8LBqpyeJl7RaHR45CoYUpc9Iyv5gwQ2YxGoHoh9DZuTjfVjuXl1Uqkv06DeNgRzkgDbuBS3XD00WHNOWlIC',
-        'client_secret': 'test client sectret',
+        'client_secret': intent.client_secret,
         }
-    
+
     return render(request, template, context)
