7291fe7da6d4f017039ed6c90acede655c663f9c
gretazas <esminis@yahoo.com>
2023-01-11 18:46:56 +0000

    Add checkout form data catching in payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -15,6 +15,9 @@ class StripeWH_Handler:
 
     def handle_payment_intent_succeeded(self, event):
         """ Payment_intent.succeeded webhook from Stripe """
+
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -12,7 +12,6 @@ import stripe
 @csrf_exempt
 def webhook(request):
     """ Listen for webhooks from Stripe """
-
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -31,7 +30,6 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
-
     # Set up a webhook handler:
     handler = StripeWH_Handler(request)
 
@@ -43,5 +41,5 @@ def webhook(request):
     event_type = event['type']
     event_handler = event_map.get(event_type, handler.handle_event)
     response = event_handler(event)
-    print('success!')
+
     return response
