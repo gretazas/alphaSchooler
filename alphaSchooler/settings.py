823ee4417c48bd1ce145bee79c18dca244f05a95
gretazas <esminis@yahoo.com>
2023-02-27 09:37:05 +0000

    Reset  AWS configuration keys

--- alphaSchooler/settings.py
+++ alphaSchooler/settings.py
@@ -201,11 +201,12 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'alphaSchooler'
+    AWS_STORAGE_BUCKET_NAME = 'alphaschooler'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_DEFAULT_ACL = 'public-read'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
