b2bd6181f739e3fe6e4a0ab7f82e6d6599889c1e
gretazas <esminis@yahoo.com>
2023-03-02 09:51:04 +0000

    Uncomment points app so could sort chechout payment system

--- alphaSchooler/settings.py
+++ alphaSchooler/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['aschooler.herokuapp.com', 'localhost']
 
@@ -190,7 +190,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- points/urls.py
+++ points/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
 
-urlpatterns = [
-    path('points/', views.points, name='points'),
-]
+# urlpatterns = [
+#     path('points/', views.points, name='points'),
+# ]
