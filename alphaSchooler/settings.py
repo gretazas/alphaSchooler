9b0655c3146e724228c1ce0d50c278621f20bc60
gretazas <esminis@yahoo.com>
2023-03-22 14:56:46 +0000

    DEBUG False

--- alphaSchooler/settings.py
+++ alphaSchooler/settings.py
@@ -28,9 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
-
-# DEBUG = 'DEBUG' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['aschooler.herokuapp.com', 'localhost']
 
