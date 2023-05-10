eb2aa19ff951905a7d167c88a3c886561bf7b012
gretazas <esminis@yahoo.com>
2023-05-10 21:18:38 +0000

    Change code in index views to make personalized greetings work

--- home/views.py
+++ home/views.py
@@ -8,40 +8,38 @@ def index(request):
     """ A view to return the index page """
     products = Product.objects.all()
     categories = None
-    today = datetime.date(2023, 12, 15)
+    username = request.user
+    dated_message = ''
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    # Add a dictionary mapping event dates to messages
-    event_messages = {
-        datetime.datetime(date.today().year, 12, 1).date(): "Happy Christmas dear {username} !!!",
-        datetime.datetime(date.today().year, 1, 6).date(): "Welcome to alphaSchooler!",
-        datetime.datetime(date.today().year, 2, 7).date(): "Happy Valentine`s day dear {username} !!!",
-        datetime.datetime(date.today().year, 2, 15).date(): "Welcome to alphaSchooler!",
-        datetime.datetime(date.today().year, 3, 10).date(): "Happy Patrick`s day dear {username} !!!",
-        datetime.datetime(date.today().year, 3, 18).date(): "Welcome to alphaSchooler!",
-        datetime.datetime(2023, 3, 18).date(): "Happy Mother`s day!",
-        datetime.datetime(2023, 3, 20).date(): "Welcome to alphaSchooler!",
-        datetime.datetime(2023, 4, 4).date(): "Happy Easter dear {username} !!!",
-        datetime.datetime(2023, 4, 10).date(): "Welcome to alphaSchooler!",
-        datetime.datetime(2024, 3, 8).date(): "Happy Mother`s day!",
-        datetime.datetime(2024, 3, 11).date(): "Welcome to alphaSchooler!",
-        datetime.datetime(2024, 3, 20).date(): "Happy Easter dear {username} !!!",
-        datetime.datetime(2024, 4, 1).date(): "Welcome to alphaSchooler!",
-        datetime.datetime(date.today().year, 8, 10).date(): "Welcome to alphaSchooler! Back to school!!!",
-        datetime.datetime(date.today().year, 9, 15).date(): "Welcome to alphaSchooler!",
-        datetime.datetime(date.today().year, 9, 22).date(): "Happy Heloween dear {username} !!!",
-        datetime.datetime(date.today().year, 11, 1).date(): "Welcome to alphaSchooler!",
-    }
+    current_date = date(2023, 12, 15)
 
-    # Get the message for the current date, and substitute the username if user is authenticated
-    message = event_messages.get(date.today(), "Welcome to alphaSchooler!")
     if request.user.is_authenticated:
-        message = message.format(username=request.user.username.capitalize())
-    print(today)  # Here we print the value of today using the print() function
+        username = request.user.username.capitalize()
+    else:
+        username = 'Guest'
+
+    if current_date.month == 12 and current_date.day >= 1 or current_date.month == 1 and current_date.day <= 6:
+        message = f"Happy Christmas dear {username} !!!"
+    elif current_date.month == 2 and current_date.day >= 7 and current_date.day <= 15:
+        message = f"Happy Valentine's day dear {username} !!!"
+    elif current_date.month == 3 and current_date.day >= 10 and current_date.day <= 18:
+        message = f"Happy Patrick's day dear {username} !!!"
+    elif current_date.month == 3 and current_date.day >= 18 and current_date.day <= 20:
+        message = f"Happy Mother's day!"
+    elif current_date.month == 4 and current_date.day >= 4 and current_date.day <= 10:
+        message = f"Happy Easter dear {username} !!!"
+    elif current_date.month == 8 and current_date.day >= 10 or current_date.month == 9 and current_date.day <= 15:
+        message = f"Welcome to alphaSchooler! Back to school!!!"
+    elif current_date.month == 9 and current_date.day >= 22 or current_date.month == 11 and current_date.day <= 1:
+        message = f"Happy Heloween dear {username} !!!"
+    else:
+        message = ""
+
     context = {
         'current_categories': categories,
         'products': products,
