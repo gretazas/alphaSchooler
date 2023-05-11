ffb825660c3a1725bbcf236c887000f619e0d3c7
gretazas <esminis@yahoo.com>
2023-05-11 16:50:10 +0000

    Add all sizes and pencil softness functionality to contex.py and add_bag views

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,7 +5,6 @@ from products.models import Product
 
 
 def bag_contents(request):
-    """ View that renders bag contents """
 
     bag_items = []
     total = 0
@@ -22,9 +21,20 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDART_DELIVERY_PERCENTAGE / 10)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
@@ -41,4 +51,5 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
+
     return context
